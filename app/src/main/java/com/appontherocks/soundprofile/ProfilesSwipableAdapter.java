dbbb904d386a0cf484852c83c40b42dc40566ce0
mhrpatel12 <mhrpatel12@gmail.com>
2017-05-07 16:17:52 +0530

    Dark themed google maps static images.

--- app/src/main/java/com/appontherocks/soundprofile/ProfilesSwipableAdapter.java
+++ app/src/main/java/com/appontherocks/soundprofile/ProfilesSwipableAdapter.java
@@ -56,8 +56,17 @@ public class ProfilesSwipableAdapter extends ArrayAdapter<SoundProfile> {
                 + ","
                 + card.longitude
                 + "&sensor=false";
+
+        String getMapURLBlack = "https://maps.googleapis.com/maps/api/staticmap"
+                + "?zoom=16&size=300x600&scale=2&markers=size:mid|color:red|"
+                + card.latitude
+                + ","
+                + card.longitude
+                + "&sensor=false"
+                + "&style=element:geometry%7Ccolor:0x242f3e&style=element:labels.text.fill%7Ccolor:0x746855&style=element:labels.text.stroke%7Ccolor:0x242f3e&style=feature:administrative.locality%7Celement:labels.text.fill%7Ccolor:0xd59563&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0xd59563&style=feature:poi.park%7Celement:geometry%7Ccolor:0x263c3f&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x6b9a76&style=feature:road%7Celement:geometry%7Ccolor:0x38414e&style=feature:road%7Celement:geometry.stroke%7Ccolor:0x212a37&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x9ca5b3&style=feature:road.highway%7Celement:geometry%7Ccolor:0x746855&style=feature:road.highway%7Celement:geometry.stroke%7Ccolor:0x1f2835&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0xf3d19c&style=feature:transit%7Celement:geometry%7Ccolor:0x2f3948&style=feature:transit.station%7Celement:labels.text.fill%7Ccolor:0xd59563&style=feature:water%7Celement:geometry%7Ccolor:0x17263c&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x515c6d&style=feature:water%7Celement:labels.text.stroke%7Ccolor:0x17263c";
+
         Glide.with(mContext)
-                .load(getMapURL)
+                .load(getMapURLBlack)
                 .into(((ImageView) view.findViewById(R.id.map_lite)));
 
         ((TextView) view.findViewById(R.id.txtProfileName)).setText(card.profileName);

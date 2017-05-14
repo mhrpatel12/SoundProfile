2f1ddd67752d157974dd01e138aa7a4ef09a5f53
Mihir <mhrpatel12@gmail.com>
2017-05-14 22:26:14 +0530

    Advanced settings activity.

---
+++ app/src/main/java/com/appontherocks/soundprofile/AdvancedSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.appontherocks.soundprofile;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AdvancedSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advanced_settings);
+    }
+}

20d35b042e12b65fafbb6234e64335f8a4b9e088
Mihir <mhrpatel12@gmail.com>
2017-05-14 22:36:26 +0530

    Change wi-fi state of device dynamically.

--- app/src/main/java/com/appontherocks/soundprofile/AdvancedSettingsActivity.java
+++ app/src/main/java/com/appontherocks/soundprofile/AdvancedSettingsActivity.java
@@ -1,7 +1,11 @@
 package com.appontherocks.soundprofile;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.widget.CompoundButton;
 
 public class AdvancedSettingsActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class AdvancedSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advanced_settings);
+
+        final WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        ((AppCompatCheckBox) findViewById(R.id.chkAutoWifi)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    wifiManager.setWifiEnabled(true);
+                } else {
+                    wifiManager.setWifiEnabled(true);
+                }
+            }
+        });
     }
 }

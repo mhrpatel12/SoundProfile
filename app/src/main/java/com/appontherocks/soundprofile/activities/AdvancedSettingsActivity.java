5e846221b084ee5482c7ec03e3c1a7574951b027
Mihir <mhrpatel12@gmail.com>
2017-05-24 22:03:11 +0530

    Dynamic radius for geofence.

--- app/src/main/java/com/appontherocks/soundprofile/activities/AdvancedSettingsActivity.java
+++ app/src/main/java/com/appontherocks/soundprofile/activities/AdvancedSettingsActivity.java
@@ -1,9 +1,19 @@
 package com.appontherocks.soundprofile.activities;
 
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatCheckBox;
 import android.support.v7.widget.Toolbar;
@@ -12,11 +22,50 @@ import android.text.TextWatcher;
 import android.widget.CompoundButton;
 
 import com.appontherocks.soundprofile.R;
+import com.appontherocks.soundprofile.Utility.Constants;
+import com.appontherocks.soundprofile.models.SoundProfile;
+import com.appontherocks.soundprofile.service.GeofenceTransitionsIntentService;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class AdvancedSettingsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class AdvancedSettingsActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, ResultCallback<Status>, GoogleApiClient.OnConnectionFailedListener {
+
+    private final int STEP_ONE_COMPLETE = 0;
     SharedPreferences.Editor advancedSettingsEdit;
     SharedPreferences advancedSettings;
+    List<Geofence> mGeofenceList;
+    private ArrayList<SoundProfile> profileArrayList = new ArrayList<>();
+    private GoogleApiClient mGoogleApiClient;
+    private Context mContext;
+    private int radius;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case STEP_ONE_COMPLETE:
+                    LocationManager manager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+                    if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                        if (mGeofenceList.size() > 0) {
+                            new saveGeoFerncesAyncTask().execute();
+                        }
+                    }
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +80,22 @@ public class AdvancedSettingsActivity extends AppCompatActivity {
         advancedSettingsEdit = getSharedPreferences(getString(R.string.advanced_settings), MODE_PRIVATE).edit();
         advancedSettings = AdvancedSettingsActivity.this.getSharedPreferences(AdvancedSettingsActivity.this.getString(R.string.advanced_settings), MODE_PRIVATE);
 
+        mContext = AdvancedSettingsActivity.this;
+
+        mGeofenceList = new ArrayList<Geofence>();
+        advancedSettings = mContext.getSharedPreferences(mContext.getString(R.string.advanced_settings), MODE_PRIVATE);
+        radius = advancedSettings.getInt(mContext.getString(R.string.geofence_radius), 50);
+        final LocationManager manager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(mContext)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        mGoogleApiClient.connect();
+
         ((AppCompatCheckBox) findViewById(R.id.chkAutoWifi)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -42,8 +107,9 @@ public class AdvancedSettingsActivity extends AppCompatActivity {
         ((AppCompatCheckBox) findViewById(R.id.chkGeofenceRadius)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Intent intent = new Intent("android.intent.action.BOOT_COMPLETED");
-                sendBroadcast(intent);
+                if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    fetchGeoFences();
+                }
             }
         });
 
@@ -71,5 +137,134 @@ public class AdvancedSettingsActivity extends AppCompatActivity {
         ((TextInputEditText) findViewById(R.id.edtGeofenceRadius)).setText(advancedSettings.getInt(getString(R.string.geofence_radius), 50) + "");
 
         ((AppCompatCheckBox) findViewById(R.id.chkAutoWifi)).setChecked(advancedSettings.getBoolean(getString(R.string.auto_disable_wifi), false));
+    }
+
+    private void fetchGeoFences() {
+        Thread backgroundThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    FirebaseDatabase.getInstance().getReference().child("profiles").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            profileArrayList.clear();
+                            for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                                SoundProfile profile = ds.getValue(SoundProfile.class);
+                                if ((profile.mKey != null) && (profile.latitude != null) && (profile.longitude != null) && !((profile.latitude + "").equals("")) && !((profile.longitude + "").equals(""))) {
+                                    mGeofenceList.add(new Geofence.Builder()
+                                            // Set the request ID of the geofence. This is a string to identify this
+                                            // geofence.
+                                            .setRequestId(profile.mKey)
+                                            .setCircularRegion(
+                                                    Double.parseDouble(profile.latitude),
+                                                    Double.parseDouble(profile.longitude),
+                                                    radius
+                                            )
+                                            .setExpirationDuration(Constants.GEOFENCE_EXPIRATION_TIME)
+                                            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                                                    Geofence.GEOFENCE_TRANSITION_EXIT)
+                                            .build());
+                                }
+                            }
+                            Message msg = Message.obtain();
+                            msg.what = STEP_ONE_COMPLETE;
+                            handler.sendMessage(msg);
+                            FirebaseDatabase.getInstance().getReference().child("profiles").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).removeEventListener(this);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                } catch (Exception e) {
+                }
+            }
+        };
+        backgroundThread.start();
+
+    }
+
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+
+
+        builder.addGeofences(mGeofenceList);
+        return builder.build();
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+/*        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }*/
+        Intent intent = new Intent(mContext, GeofenceTransitionsIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        return PendingIntent.getService(mContext, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onResult(@NonNull Status status) {
+
+    }
+
+    public class saveGeoFerncesAyncTask extends AsyncTask<Void, String, String> {
+
+        /**
+         * Before starting background thread
+         * Show Progress Bar Dialog
+         */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        /**
+         * Downloading file in background thread
+         */
+        @Override
+        protected String doInBackground(Void... f_url) {
+            if (ActivityCompat.checkSelfPermission(mContext, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
+
+            LocationServices.GeofencingApi.addGeofences(
+                    mGoogleApiClient,
+                    getGeofencingRequest(),
+                    getGeofencePendingIntent()
+            ).setResultCallback(AdvancedSettingsActivity.this);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+        }
     }
 }

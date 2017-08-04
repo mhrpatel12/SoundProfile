eb9dc434638a0edb05899f301b74d1939b2e04af
Mihir <mhrpatel12@gmail.com>
2017-08-04 15:39:22 +0530

    Interface for showing alert of discarding/saving default profile changes.

---
+++ app/src/main/java/com/appontherocks/soundprofile/interfaces/DefaultProfileInterface.java
@@ -0,0 +1,11 @@
+package com.appontherocks.soundprofile.interfaces;
+
+/**
+ * Created by patelmih on 8/4/2017.
+ */
+
+public class DefaultProfileInterface {
+    public interface AlertForDiscardDefaultProfileChanges {
+        void alertForDiscardDefaultProfileChanges();
+    }
+}

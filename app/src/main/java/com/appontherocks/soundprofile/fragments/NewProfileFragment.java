f9880d7ce8d97883bd9a54dcfa47ca993430405d
Mihir <mhrpatel12@gmail.com>
2017-08-11 18:39:41 +0530

    Fragment for creating new new sound profile

--- app/src/main/java/com/appontherocks/soundprofile/fragments/NewProfileFragment.java
+++ app/src/main/java/com/appontherocks/soundprofile/fragments/NewProfileFragment.java
@@ -459,13 +459,6 @@ public class NewProfileFragment extends Fragment implements GoogleApiClient.Conn
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         if (ActivityCompat.checkSelfPermission(mContext, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(

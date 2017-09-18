960fcf5e405c9493f7e8e6cc6b0bdfd22be4f3d6
Mihir <mhrpatel12@gmail.com>
2017-09-18 17:54:34 +0530

    Bottomsheet time picker for adding sleepy hours

--- app/src/main/java/com/appontherocks/soundprofile/fragments/SleepyHoursFragment.java
+++ app/src/main/java/com/appontherocks/soundprofile/fragments/SleepyHoursFragment.java
@@ -1,24 +1,39 @@
 package com.appontherocks.soundprofile.fragments;
 
-import android.app.TimePickerDialog;
+import android.app.ActivityManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatDrawableManager;
 import android.support.v7.widget.AppCompatTextView;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.TimePicker;
 
 import com.appontherocks.soundprofile.R;
+import com.appontherocks.soundprofile.service.SleepyHoursService;
+import com.philliphsu.bottomsheetpickers.time.BottomSheetTimePickerDialog;
+import com.philliphsu.bottomsheetpickers.time.grid.GridTimePickerDialog;
 
 import java.util.Calendar;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SleepyHoursFragment extends Fragment {
+public class SleepyHoursFragment extends Fragment implements BottomSheetTimePickerDialog.OnTimeSetListener {
 
     private Context mContext;
     private View view;
@@ -28,9 +43,22 @@ public class SleepyHoursFragment extends Fragment {
 
     private AppCompatTextView txtStartTime;
     private AppCompatTextView txtEndTime;
+    private AppCompatTextView selectedTextView;
+
+    private boolean isServiceRunning = false;
+
+    private AppCompatButton btnEnableSleepyHours;
+    private int startHour, startMinute = 0;
+    private int endHour, endMinute = 0;
 
     public SleepyHoursFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -38,6 +66,10 @@ public class SleepyHoursFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_sleepy_hours, container, false);
         mContext = view.getContext();
+
+        btnEnableSleepyHours = (AppCompatButton) view.findViewById(R.id.btnEnableSleepyHours);
+        Drawable somevectordrable = AppCompatDrawableManager.get().getDrawable(mContext, R.drawable.ic_brightness_3_black_24dp);
+        btnEnableSleepyHours.setCompoundDrawables(somevectordrable, null, null, null);
 
         layoutStartTime = (LinearLayout) view.findViewById(R.id.layoutStartHours);
         layoutStartTime.setOnClickListener(new View.OnClickListener() {
@@ -57,24 +89,97 @@ public class SleepyHoursFragment extends Fragment {
         txtStartTime = (AppCompatTextView) view.findViewById(R.id.txtStartTime);
         txtEndTime = (AppCompatTextView) view.findViewById(R.id.txtEndTime);
 
+        btnEnableSleepyHours.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if ((startHour == 0)
+                        || (startMinute) == 0
+                        || (endHour) == 0
+                        || (endMinute) == 0) {
+                    return;
+                }
+
+                ActivityManager manager = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+                isServiceRunning = false;
+                for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+                    if (SleepyHoursService.class.getName().equals(service.service.getClassName())) {
+                        isServiceRunning = true;
+                    }
+                }
+
+                if (isServiceRunning) {
+                    SharedPreferences prefs = mContext.getSharedPreferences(getString(R.string.sleep_hours), MODE_PRIVATE);
+                    int startHour = prefs.getInt("startHour", 0); //0 is the default value.
+                    int startMinute = prefs.getInt("startMinute", 0); //0 is the default value.
+                    int endHour = prefs.getInt("endHour", 0); //0 is the default value.
+                    int endMinute = prefs.getInt("endMinute", 0); //0 is the default value.
+
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                    builder.setTitle(getResources().getString(R.string.sleep_hours))
+                            .setMessage(getResources().getString(R.string.sleep_hours_warning_start) + " " + startHour + ":" + startMinute + " to " + endHour + ":" + endMinute
+                                    + getResources().getString(R.string.sleep_hours_warning_end))
+                            .setCancelable(true)
+                            .setPositiveButton("Remove", new DialogInterface.OnClickListener() {
+                                public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                                    mContext.stopService(new Intent(mContext, SleepyHoursService.class));
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                                    dialog.cancel();
+                                }
+                            });
+                    final AlertDialog alert = builder.create();
+                    alert.show();
+                } else {
+                    SharedPreferences.Editor editor = mContext.getSharedPreferences(getString(R.string.sleep_hours), MODE_PRIVATE).edit();
+                    editor.putInt("startHour", startHour);
+                    editor.putInt("startMinute", startMinute);
+                    editor.putInt("endHour", endHour);
+                    editor.putInt("endMinute", endMinute);
+                    editor.commit();
+                    Intent intent = new Intent(mContext, SleepyHoursService.class);
+                    intent.putExtra("startHour", startHour);
+                    intent.putExtra("startMinute", startMinute);
+                    intent.putExtra("endHour", endHour);
+                    intent.putExtra("endMinute", endMinute);
+                    mContext.startService(intent);
+                }
+            }
+        });
+
         return view;
     }
 
-    Calendar mcurrentTime = Calendar.getInstance();
-    final int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-    final int minute = mcurrentTime.get(Calendar.MINUTE);
-
     private void showTimeDialog(AppCompatTextView textView, String time) {
-        final AppCompatTextView mTextView = textView;
+        selectedTextView = textView;
+        Calendar now = Calendar.getInstance();
 
-        TimePickerDialog mTimePicker;
-        mTimePicker = new TimePickerDialog(mContext, new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                mTextView.setText(selectedHour + " : " + selectedMinute);
+        GridTimePickerDialog grid = new GridTimePickerDialog.Builder(
+                this,
+                now.get(Calendar.HOUR_OF_DAY),
+                now.get(Calendar.MINUTE),
+                DateFormat.is24HourFormat(mContext))
+                .build();
+        int[] attrs = new int[]{R.attr.drawable_color_inverse};
+        TypedArray ta = mContext.obtainStyledAttributes(attrs);
+
+        grid.setAccentColor(ta.getColor(0, 0));
+        grid.show(getActivity().getSupportFragmentManager(), "SleepyHoursFragment");
+    }
+
+    @Override
+    public void onTimeSet(ViewGroup viewGroup, int hourOfDay, int minute) {
+        if (selectedTextView != null) {
+            if (selectedTextView == txtStartTime) {
+                startHour = hourOfDay;
+                startMinute = minute;
+            } else if (selectedTextView == txtEndTime) {
+                endHour = hourOfDay;
+                endMinute = minute;
             }
-        }, hour, minute, true);//Yes 24 hour time
-        mTimePicker.setMessage(time);
-        mTimePicker.show();
+            selectedTextView.setText(hourOfDay + " : " + minute);
+        }
     }
 }
